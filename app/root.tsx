6ea6d38f26d5452c362b1270005404099abd8e6f
Devin Santamaria <devin@webprism.co>
2024-03-27 17:45:20 -0700

    init test project

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import styles from "./app.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
